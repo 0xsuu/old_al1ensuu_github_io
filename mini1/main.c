223abf3817d21c41ecd38927a97ad7ca3657102b
H Sun <hs1145@pc753s.cs.york.ac.uk>
2015-10-15 11:40:43 +0100

    update mini1

--- mini1/main.c
+++ mini1/main.c
@@ -4,48 +4,13 @@
 #include "main.h"
 #include "led.h"
 #include "serialIO.h"
-
-void SysTick_Handler(void)
-{
-    LEDdebug(0b1111);
-    //Clear System Tick counter flag
-    SYSTICK_ClearCounterFlag();
-
-    if (Cur_State == ENABLE)
-    {
-       //GPIO_SetValue(1, LED1 | LED2 | LED3 | LED4);
-       LEDdebug(0b0110);
-       Cur_State = DISABLE;
-    }
-    else
-    {
-       //GPIO_ClearValue(1, LED1 | LED2 | LED3 | LED4);
-       LEDoff();
-       Cur_State = ENABLE;
-    }
-}
+#include "sysTick.h"
 
 void delay() 
 {
    unsigned int i;
  
    for (i = 0; i < 0x800000; i++);
-}
-
-void delayS(uint32_t time)
-{
-	LEDdebug(0b1000);
-	delay();
-    //Initialize System Tick with some time interval
-    SYSTICK_InternalInit(200);
-    //Enable System Tick interrupt
-    //SYSTICK_IntCmd(ENABLE);
-    //Enable System Tick Counter
-    LEDdebug(0b1100);
-    delay();
-    SYSTICK_Cmd(ENABLE);
-    LEDdebug(0b1110);
-    delay();
 }
 
 void stage1()
@@ -74,15 +39,16 @@ void stage2()
 
 void stage3()
 {
-    //LEDdebugR(0b1001);
-    delayS(100);
+    LEDdebugR(0b1001);
+    delayMS(1000);
     LEDoff();
 }
 
 void stage4()
 {
+	LEDdebugR(0b1001);
 	serial_init();
-	write_usb_serial_blocking("Hello World!\n\r",16);
+	printWithInt("Hello World! %\n\r", 0x12345678);
 }
 
 int main()
